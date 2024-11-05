74f18f640f6bb98bddcce09c44d90c5feab35637
MMontoya2511 <121247550+MMontoya2511@users.noreply.github.com>
2024-11-05 15:00:15 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -7,10 +7,10 @@ def main():
     print("Calculadora de Resta:")
     print("1. Resta")
 
-    opcion = int(input("Elige una operación (1 para Resta): "))
+    opcion = int(input("Elige una operación (2 para Resta): "))
 
     # Solo permite continuar si se selecciona la opción 1
-    if opcion == 1:
+    if opcion == 2:
         a = float(input("Ingresa el primer número: "))
         b = float(input("Ingresa el segundo número: "))
         print("Resultado:", resta(a, b))
