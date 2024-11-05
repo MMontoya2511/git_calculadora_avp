70a98140c87b80367b697ab37fda09448fafec6d
MMontoya2511 <121247550+MMontoya2511@users.noreply.github.com>
2024-11-05 14:57:25 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,23 @@
-# calculadora.py
+# calculadoraResta.py
 
+def resta(a, b):
+    return a - b
+
+def main():
+    print("Calculadora de Resta:")
+    print("1. Resta")
+
+    opcion = int(input("Elige una operación (1 para Resta): "))
+
+    # Solo permite continuar si se selecciona la opción 1
+    if opcion == 1:
+        a = float(input("Ingresa el primer número: "))
+        b = float(input("Ingresa el segundo número: "))
+        print("Resultado:", resta(a, b))
+    else:
+        print("Opción no válida")
+
+if __name__ == "__main__":
+    main()
 
 
